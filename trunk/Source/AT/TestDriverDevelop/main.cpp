19bc787a42c32281f2d6f015c812df5a1c49ed0a
Xiong Yang <snake.xiongyang@qq.com>
2012-12-04 01:41:35 +0800

    -add gmock
    Signed-off-by: Xiong Yang <snake.xiongyang@qq.com>

---
+++ trunk/Source/AT/TestDriverDevelop/main.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+
+int main(int argc,char** argv)
+{
+	::testing::InitGoogleMock(&argc, argv);
+	 RUN_ALL_TESTS();
+	 system("pause");
+	 return 0;
+
+}
